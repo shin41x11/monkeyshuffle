77ef2db52f257f54a23c88ea6e8951d0c6e43e3d
s-yokoi <s-yokoi@dxg.jp>
2020-07-09 15:46:44 +0900

    作業中保存。猿の木間の移動が機能する。
    木のプレハブ修正前。

--- monkeyshuffule_unity/Assets/Scripts/Treering.cs
+++ monkeyshuffule_unity/Assets/Scripts/Treering.cs
@@ -12,6 +12,9 @@ public class Treering : MonoBehaviour
 
     private Rigidbody rb; // 自身のrigidBody。再利用用に参照を保存。
 
+    [SerializeField]
+    private float RotationPower = 10f; // 回転のパワー
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,21 +28,16 @@ public class Treering : MonoBehaviour
         rotate();
         buttonForce();
     }
-    //
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.gameObject.name); // ぶつかった相手の名前を取得
-
-        // 猿にぶつかったら、猿を子オブジェクトにする
-        collision.gameObject.transform.parent = gameObject.transform;
-
-    }
 
     // 自動回転 
     // @todo 定期的に逆回転させる
     void rotate()
     {
-        
+
+        // 物理で回す版
+        //rb.AddTorque(0f, RotationPower, 0f);
+
+        // 角度をスクリプトで変更する版
         var angle = this.gameObject.transform.localEulerAngles;
         angle.y += Time.deltaTime * 60.0f;
         this.gameObject.transform.localEulerAngles = angle;
