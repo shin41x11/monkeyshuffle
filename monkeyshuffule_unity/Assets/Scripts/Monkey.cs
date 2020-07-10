2429fedad672d80c58177ca4870d4a4271d4a487
s-yokoi <s-yokoi@dxg.jp>
2020-07-10 17:40:20 +0900

    作業中保存。 prefabのcollision再設定。
    猿が木から離れづらくなっているのを要調整

---
+++ monkeyshuffule_unity/Assets/Scripts/Monkey.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Monkey : MonoBehaviour
+{
+    private Dictionary<string, GameObject> collisionDict; // 衝突中オブジェクト辞書。keyにobject名(tree1等), valueにgameobjectを持つ
+    private Rigidbody rb; // rigidBody。再利用用に参照を保存。
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionDict = new Dictionary<string, GameObject>();
+
+        // GetComponentは重たいので最初に呼んでおく
+        rb = gameObject.transform.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collisionDict.Count == 1)
+        {
+            if (rb.isKinematic == false)
+            {
+                Debug.Log("captured:"); // ぶつかった
+                gameObject.transform.parent = collisionDict.First().Value.transform;
+                rb.isKinematic = true;
+            }
+        }
+        else
+        {
+            if (rb.isKinematic == true)
+            {
+                Debug.Log("released:"); // 解放された
+                gameObject.transform.parent = null;
+                rb.isKinematic = false;
+            }
+        }
+    }
+
+    public void relayOnTriggerEnter(Collider collider)
+    {
+        string collisionObjectName = collider.gameObject.transform.parent.name;
+
+        if (collisionDict.ContainsKey(collisionObjectName) == false)
+        {
+            collisionDict.Add(collisionObjectName, collider.gameObject);
+            Debug.Log("Add:" + collisionObjectName); // ぶつかった相手の名前を取得
+        }
+    }
+
+
+
+    public void relayOnTriggerExit(Collider collider)
+    {
+        string collisionObjectName = collider.gameObject.transform.parent.name;
+
+        if (collisionDict.ContainsKey(collisionObjectName))
+        {
+            collisionDict.Remove(collisionObjectName);
+            Debug.Log("Remove:" + collisionObjectName); // ぶつかった相手の名前を取得
+        }
+    }
+}
---
+++ monkeyshuffule_unity/Assets/Scripts/MonkeyArm.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 猿の両手の引っかかる部分向けのスクリプト
+// OnCollision系関数を親に渡すのみ
+public class MonkeyArm : MonoBehaviour
+{
+
+    private Monkey parentObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentObject = gameObject.transform.parent.gameObject.GetComponent<Monkey>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("enter:"); // ぶつかった
+        if (collider.transform.CompareTag("TreeRing"))
+        {
+            parentObject.relayOnTriggerEnter(collider);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("exit:"); // ぶつかった
+        if (collider.transform.CompareTag("TreeRing"))
+        {
+            parentObject.relayOnTriggerExit(collider);
+        }
+    }
+}
